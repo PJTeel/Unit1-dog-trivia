219a2c3095164221ac1cbf4879604faa105b8d23
Paula Teel <teelpaulaj@johndeere.com>
2021-01-19 21:34:59 -0600

    Question 1

---
+++ script.js
@@ -0,0 +1,18 @@
+//console.log("hello")
+let playChoice
+let ansCorrect = 0;
+let ansWrong = 0;
+
+var quiz ={
+    data: [
+    {
+     question: "Which breed group has won Best of Show most often at the Westminster Dog Show?",
+     options: [
+            "Sporting Group",
+            "Terrier Group",
+            "Toy Group",
+            "Hound Group"
+             ],
+    answer: 1 //Terrier Group
+    }
+]
