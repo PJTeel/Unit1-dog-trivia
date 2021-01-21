6b945063787a4f128cd8cd9e0b3bf58d75742604
Paula Teel <teelpaulaj@johndeere.com>
2021-01-21 10:58:11 -0600

    Scoring display on clean page

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 function check() {
-	//document.write("Never give up!");
 	var c=0;
 	var q1=document.quiz.question1.value;
 	if (q1=="Terrier Group") {c++}
 	var q2=document.quiz.question2.value;
 	if (q2=="42") {c++}
-	document.write(c)
-	var q3=document.quiz.question3.value;
+		var q3=document.quiz.question3.value;
 	if (q3=="195") {c++}
-	document.write(c)
 	var q4=document.quiz.question4.value;
 	if (q4=="Labrador") {c++}
-	document.write(c)
 	var q5=document.quiz.question5.value;
 	if (q5=="Greyhound") {c++}
-	document.write(c)
 	var q6=document.quiz.question6.value;
 	if (q6=="Luna") {c++}
-	document.write(c)
 	var q7=document.quiz.question7.value;
 	if (q7=="29") {c++}
-	document.write(c)
 	var q8=document.quiz.question8.value;
 	if (q8=="True") {c++}
-	document.write(c)
 	var q9=document.quiz.question9.value;
 	if (q9=="False") {c++}
-	document.write(c)
 	var q10=document.quiz.question10.value;
 	if (q10=="True") {c++}
-	document.write(c)
+	//document.write(c)
+
+	var quiz=document.getElementById('quiz');
+	quiz.style.display="none" //clears the screen before displaying the results
+
+	var result=document.getElementById('result');
+	result.textContent=(c);
+
 }
+
